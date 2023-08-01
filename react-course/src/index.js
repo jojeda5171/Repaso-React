c2f26e3810efcf1e794328d3b7508a2971294943
José Ojeda <jojeda5171@uta.edu.ec>
2023-08-01 14:03:55 -0500

    jsx

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,18 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const rootElement = document.getElementById('root')
-const root = ReactDOM.createRoot(rootElement)
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
 
 function Greeting() {
-    return <div>
-        <h1>Este es un componente</h1>
-        <p>lorem 123</p>
+  function add(a, b) {
+    return a + b;
+  }
+
+  //const name ='José'
+  const user = {
+    firstName: "Ryan",
+    lastName: "Ray",
+  };
+  return (
+    <div>
+      <h1>{user.firstName}</h1>
+      <h3>{user.lastName}</h3>
+      <h1>{add(10, 30)}</h1>
     </div>
+  );
 }
 
-root.render(<div>
+root.render(
+  <>
     <Greeting></Greeting>
     <Greeting></Greeting>
     <Greeting></Greeting>
-</div>)
+  </>
+);
