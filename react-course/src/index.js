41958d7b3802798229e6217c4ea94db2311ba2fe
José Ojeda <jojeda5171@uta.edu.ec>
2023-08-01 13:46:38 -0500

    mi primer coponente

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -4,4 +4,15 @@ import ReactDOM from 'react-dom/client'
 const rootElement = document.getElementById('root')
 const root = ReactDOM.createRoot(rootElement)
 
-root.render(<h1>Hola Mundo</h1>)
+function Greeting() {
+    return <div>
+        <h1>Este es un componente</h1>
+        <p>lorem 123</p>
+    </div>
+}
+
+root.render(<div>
+    <Greeting></Greeting>
+    <Greeting></Greeting>
+    <Greeting></Greeting>
+</div>)
